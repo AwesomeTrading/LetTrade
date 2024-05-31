b1dc73839d9f863025e33103526d3d14fa282ffb
Santatic <namns.dev@gmail.com>
2024-05-31 16:09:33 +0700

    WIP: 2 Implement of datefeed base on datetimeindex and rangedindex

--- lettrade/data/data.py
+++
@@ -1,4 +0,0 @@
-from .base.base import TimeFrame
-from .base.data_datetimeindex import DataFeed, DataFeedIndex
-
-# from .base.data_rangeindex import DataFeed, DataFeedIndex
---
+++ lettrade/data/data/__init__.py
@@ -0,0 +1,4 @@
+from .base import TimeFrame
+from .data_datetimeindex import DataFeed, DataFeedIndex
+
+# from .data_rangeindex import DataFeed, DataFeedIndex

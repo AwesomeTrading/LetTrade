5a2c787048d8adc07131089d5a622cb161c46bd6
Santatic <namns.dev@gmail.com>
2024-06-02 17:26:47 +0700

    WIP plotly merge

--- lettrade/plot/plotly/helper.py
+++ lettrade/plot/plotly/helper.py
@@ -1,13 +1,15 @@
-def plot_merge(source: dict, update: dict):
-    for k, v in update.items():
-        if k not in source:
-            source[k] = v
-            continue
-        if isinstance(source[k], list):
-            source[k].extends(v)
-            continue
-        if isinstance(source[k], list):
-            s = source[k]
-            for k1, v1 in v:
-                s[k1] = v1
-            continue
+def plot_merge(source: dict, *updates: list[dict]):
+    for update in updates:
+        for k, v in update.items():
+            if k not in source:
+                source[k] = v
+                continue
+            if isinstance(source[k], list):
+                source[k].extend(v)
+                continue
+            if isinstance(source[k], list):
+                s = source[k]
+                for k1, v1 in v:
+                    s[k1] = v1
+                continue
+    return source

4a0f7e9e873423d341dbcf19a5005bfc1bfc4062
Santatic <namns.dev@gmail.com>
2024-06-05 13:30:35 +0700

    Plot: color

---
+++ lettrade/plot/color.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class PlotColor(str, Enum):
+    RED = "#ff1947"
+    PINK = "#f50056"
+    LIGHT_PINK = "#f06292"
+    PURPLE = "#df41fb"
+    DEEP_PURPLE = "#7c4dff"
+    INDIGO = "#526cfe"
+    BLUE = "#4287ff"
+    LIGHT_BLUE = "#0091eb"
+    CYAN = "#00bad6"
+    TEAL = "#00bda4"
+    GREEN = "#00c753"
+    LIGHT_GREEN = "#63de17"
+    LIME = "#b0eb00"
+    YELLOW = "#ffd500"
+    LIGHT_YELLOW = "#fff176"
+    AMBER = "#fa0"
+    ORANGE = "#ff9100"
+    DEEP_ORANGE = "#ff6e42"

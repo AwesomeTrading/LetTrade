e84d97eff46d124acebec73ac99a03cc12234dd0
Santatic <namns.dev@gmail.com>
2024-06-07 13:25:11 +0700

    Data: TimeFrame init & test

--- lettrade/data/timeframe.py
+++ lettrade/data/timeframe.py
@@ -1,35 +1,46 @@
 import logging
+import re
 
 import pandas as pd
 
 logger = logging.getLogger(__name__)
 
-TIMEFRAME_STR_2_DELTA = {
-    "1m": (1, "m", "m", pd.Timedelta(1, "m")),
-    "2m": (2, "m", "m", pd.Timedelta(2, "m")),
-    "3m": (3, "m", "m", pd.Timedelta(3, "m")),
-    "4m": (4, "m", "m", pd.Timedelta(4, "m")),
-    "5m": (5, "m", "m", pd.Timedelta(5, "m")),
-    "6m": (6, "m", "m", pd.Timedelta(6, "m")),
-    "10m": (10, "m", "m", pd.Timedelta(10, "m")),
-    "12m": (12, "m", "m", pd.Timedelta(12, "m")),
-    "15m": (15, "m", "m", pd.Timedelta(15, "m")),
-    "20m": (20, "m", "m", pd.Timedelta(20, "m")),
-    "30m": (30, "m", "m", pd.Timedelta(30, "m")),
-    "1h": (1, "h", "h", pd.Timedelta(1, "h")),
-    "2h": (2, "h", "h", pd.Timedelta(2, "h")),
-    "3h": (3, "h", "h", pd.Timedelta(3, "h")),
-    "4h": (4, "h", "h", pd.Timedelta(4, "h")),
-    "6h": (6, "h", "h", pd.Timedelta(6, "h")),
-    "8h": (8, "h", "h", pd.Timedelta(8, "h")),
-    "12h": (12, "h", "h", pd.Timedelta(12, "h")),
-    "1d": (1, "d", "day", pd.Timedelta(1, "day")),
-    "1w": (1, "w", "W", pd.Timedelta(1, "W")),
+
+TIMEFRAME_UNIT_LET_2_PANDAS = {
+    "s": "s",
+    "m": "m",
+    "h": "h",
+    "d": "day",
+    "w": "W",
+    # "1mn"
+}
+TIMEFRAME_UNIT_PANDAS_2_LET = {v: k for k, v in TIMEFRAME_UNIT_LET_2_PANDAS.items()}
+
+TIMEFRAME_DELTA_2_STR = {
+    pd.Timedelta(1, "m"): (1, "m"),
+    pd.Timedelta(2, "m"): (2, "m"),
+    pd.Timedelta(3, "m"): (3, "m"),
+    pd.Timedelta(4, "m"): (4, "m"),
+    pd.Timedelta(5, "m"): (5, "m"),
+    pd.Timedelta(6, "m"): (6, "m"),
+    pd.Timedelta(10, "m"): (10, "m"),
+    pd.Timedelta(12, "m"): (12, "m"),
+    pd.Timedelta(15, "m"): (15, "m"),
+    pd.Timedelta(20, "m"): (20, "m"),
+    pd.Timedelta(30, "m"): (30, "m"),
+    pd.Timedelta(1, "h"): (1, "h"),
+    pd.Timedelta(2, "h"): (2, "h"),
+    pd.Timedelta(3, "h"): (3, "h"),
+    pd.Timedelta(4, "h"): (4, "h"),
+    pd.Timedelta(6, "h"): (6, "h"),
+    pd.Timedelta(8, "h"): (8, "h"),
+    pd.Timedelta(12, "h"): (12, "h"),
+    pd.Timedelta(1, "day"): (1, "d"),
+    pd.Timedelta(1, "W"): (1, "w"),
     # "1mn": pd.Timedelta(days=30),
 }
-TIMEFRAME_DELTA_2_STR = {
-    v[3]: [v[0], v[1], v[2], k] for k, v in TIMEFRAME_STR_2_DELTA.items()
-}
+
+_pattern_timeframe_str = re.compile(f"^([0-9]+)([a-z]+)$")
 
 
 class TimeFrame:
@@ -40,30 +51,35 @@ class TimeFrame:
 
     def __init__(self, tf: str | pd.Timedelta) -> None:
         if isinstance(tf, TimeFrame):
-            self.delta = tf.delta
-            self.unit = tf.unit
-            self.unit_pandas = tf.unit_pandas
             self.value = tf.value
+            self.unit = tf.unit
         elif isinstance(tf, str):
-            map = TIMEFRAME_STR_2_DELTA[tf]
-            self.value = map[0]
-            self.unit = map[1]
-            self.unit_pandas = map[2]
-            self.delta = pd.Timedelta(map[3])
+            match = _pattern_timeframe_str.search(tf)
+            if not match:
+                raise RuntimeError(f"TimeFrame value {tf} is invalid")
+
+            unit = match.group(2)
+            if unit not in TIMEFRAME_UNIT_LET_2_PANDAS:
+                raise RuntimeError(f"TimeFrame value {tf} with unit {unit} is invalid")
+
+            self.value = int(match.group(1))
+            self.unit = unit
         elif isinstance(tf, int):
-            map = TIMEFRAME_STR_2_DELTA[f"{tf}m"]
-            self.value = map[0]
-            self.unit = map[1]
-            self.unit_pandas = map[2]
-            self.delta = pd.Timedelta(map[3])
+            self.value = tf
+            self.unit = "m"
+        elif isinstance(tf, list):
+            self.value = int(tf[0])
+            self.unit = tf[1]
         elif isinstance(tf, pd.Timedelta):
             map = TIMEFRAME_DELTA_2_STR[tf]
             self.value = map[0]
             self.unit = map[1]
-            self.unit_pandas = map[2]
-            self.delta = pd.Timedelta(tf)
         else:
             raise RuntimeError(f"Timeframe {tf} is invalid format")
+
+        # Setup
+        self.unit_pandas = TIMEFRAME_UNIT_LET_2_PANDAS[self.unit]
+        self.delta = pd.Timedelta(self.value, self.unit_pandas)
 
     def __repr__(self) -> str:
         return self.string
@@ -83,4 +99,7 @@ class TimeFrame:
         return at.floor(freq=freq)
 
     def ceil(self, at: pd.Timestamp):
-        return at.ceil(freq=self.string_pandas)
+        freq = self.string_pandas
+        if self.unit == "m":
+            freq += "in"
+        return at.ceil(freq=freq)
---
+++ test/data/timeframe.py
@@ -0,0 +1,117 @@
+import unittest
+
+import pandas as pd
+
+from lettrade.data import TimeFrame
+
+
+class DataFeedInjectTestCase(unittest.TestCase):
+    def test_int(self):
+        tf = TimeFrame(1)
+        self.assertEqual(tf.string, "1m")
+
+    def test_string(self):
+        tf = TimeFrame("3s")
+        self.assertEqual(tf.string, "3s")
+
+        tf = TimeFrame("5m")
+        self.assertEqual(tf.string, "5m")
+
+        tf = TimeFrame("2h")
+        self.assertEqual(tf.string, "2h")
+
+        tf = TimeFrame("12h")
+        self.assertEqual(tf.string, "12h")
+
+        tf = TimeFrame("1d")
+        self.assertEqual(tf.string, "1d")
+
+        tf = TimeFrame("7d")
+        self.assertEqual(tf.string, "7d")
+
+        # ERROR
+        tf = TimeFrame("12d")
+        self.assertEqual(tf.string, "12d")
+
+        tf = TimeFrame("9999d")
+        self.assertEqual(tf.string, "9999d")
+
+    def test_timedelta(self):
+        tf = TimeFrame(pd.Timedelta(minutes=3))
+        self.assertEqual(tf.string, "3m")
+
+        tf = TimeFrame(pd.Timedelta(hours=3))
+        self.assertEqual(tf.string, "3h")
+
+        tf = TimeFrame(pd.Timedelta(days=1))
+        self.assertEqual(tf.string, "1d")
+
+        tf = TimeFrame(pd.Timedelta(weeks=1))
+        self.assertEqual(tf.string, "1w")
+
+    def test_list(self):
+        tf = TimeFrame([3, "s"])
+        self.assertEqual(tf.string, "3s")
+
+        tf = TimeFrame([1, "m"])
+        self.assertEqual(tf.string, "1m")
+
+        tf = TimeFrame([7, "h"])
+        self.assertEqual(tf.string, "7h")
+
+        tf = TimeFrame([1, "w"])
+        self.assertEqual(tf.string, "1w")
+
+    def test_exception(self):
+        with self.assertRaises(RuntimeError):
+            TimeFrame("asd123")
+
+        with self.assertRaises(RuntimeError):
+            TimeFrame("1a")
+
+        with self.assertRaises(RuntimeError):
+            TimeFrame("1z")
+
+        with self.assertRaises(RuntimeError):
+            TimeFrame(pd.Timestamp("2020-01-01"))
+
+    def test_floor(self):
+        # Second
+        tf = TimeFrame("5s")
+
+        floor = tf.floor(pd.Timestamp("2020-01-01 01:00:03"))
+        self.assertEqual(floor, pd.Timestamp("2020-01-01 01:00:00"))
+
+        floor = tf.floor(pd.Timestamp("2020-01-01 01:00:23"))
+        self.assertEqual(floor, pd.Timestamp("2020-01-01 01:00:20"))
+
+        # Minute
+        tf = TimeFrame("5m")
+
+        floor = tf.floor(pd.Timestamp("2020-01-01 01:00:03"))
+        self.assertEqual(floor, pd.Timestamp("2020-01-01 01:00:00"))
+
+        floor = tf.floor(pd.Timestamp("2020-01-01 01:12:23"))
+        self.assertEqual(floor, pd.Timestamp("2020-01-01 01:10:00"))
+
+        # Hour
+        tf = TimeFrame("4h")
+
+        floor = tf.floor(pd.Timestamp("2020-01-01 11:11:03"))
+        self.assertEqual(floor, pd.Timestamp("2020-01-01 8:00:00"))
+
+        floor = tf.floor(pd.Timestamp("2020-01-01 21:12:23"))
+        self.assertEqual(floor, pd.Timestamp("2020-01-01 20:00:00"))
+
+    def test_ceil(self):
+        tf = TimeFrame("5s")
+
+        ceil = tf.ceil(pd.Timestamp("2020-01-01 01:00:03"))
+        self.assertEqual(ceil, pd.Timestamp("2020-01-01 01:00:05"))
+
+        ceil = tf.ceil(pd.Timestamp("2020-01-01 01:00:23"))
+        self.assertEqual(ceil, pd.Timestamp("2020-01-01 01:00:25"))
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)

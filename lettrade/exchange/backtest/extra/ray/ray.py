b0da5b36d176c28162a32ebb25c1b6e15f911308
Santatic <namns.dev@gmail.com>
2024-06-15 10:14:22 +0700

    Backtest: check ray module exist

--- lettrade/exchange/backtest/extra/ray/ray.py
+++ lettrade/exchange/backtest/extra/ray/ray.py
@@ -1,17 +1,19 @@
-from ray.util.queue import Queue
+from importlib.util import find_spec
 
+if find_spec("ray") is not None:
+    from ray.util.queue import Queue
 
-class LetOptimizeRay:
-    _data: dict
+    class LetOptimizeRay:
+        _data: dict
 
-    def dumps(self, data: dict, lt: "LetTradeBackTest"):
-        self._data = data
+        def dumps(self, data: dict, lt: "LetTradeBackTest"):
+            self._data = data
 
-        # Patch: overrite lettrade queue
-        q = Queue()
-        data["kwargs"]["queue"] = q
-        lt._stats._q = q
+            # Patch: overrite lettrade queue
+            q = Queue()
+            data["kwargs"]["queue"] = q
+            lt._stats._q = q
 
-    @property
-    def data(self) -> dict:
-        return self._data
+        @property
+        def data(self) -> dict:
+            return self._data

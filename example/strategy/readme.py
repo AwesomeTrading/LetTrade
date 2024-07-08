656a542ad4021f4107d1a333e48c5a730a648272
Santatic <namns.dev@gmail.com>
2024-07-08 23:10:46 +0700

    ReadMe.md

--- example/strategy/readme.py
+++ example/strategy/readme.py
@@ -1,6 +1,3 @@
-import talib.abstract as ta
-
-from lettrade import indicator as i
 from lettrade.all import DataFeed, ForexBackTestAccount, Strategy, let_backtest
 
 
@@ -9,32 +6,19 @@ class SmaCross(Strategy):
     ema2_window = 21
 
     def indicators(self, df: DataFeed):
-        df["ema1"] = ta.EMA(df, timeperiod=self.ema1_window)
+        df.i.ema(name="ema1", window=self.ema1_window, inplace=True, plot=True)
         df.i.ema(name="ema2", window=self.ema2_window, inplace=True, plot=True)
 
-        df["crossover"] = i.crossover(df.ema1, df.ema2)
+        df.i.crossover("ema1", "ema2", inplace=True, plot=True)
         df.i.crossunder("ema1", "ema2", inplace=True, plot=True)
 
     def next(self, df: DataFeed):
-        if df.l.crossover[-1]:
+        if df.l.crossover[-1] >= 100:
             self.positions_exit()
             self.buy(size=0.1)
-        elif df.l.crossunder[-1]:
+        elif df.l.crossunder[-1] <= -100:
             self.positions_exit()
             self.sell(size=0.1)
-
-    def plot(self, df: DataFeed) -> dict:
-        from lettrade.plot.plotly import PlotColor, plot_line, plot_mark, plot_merge
-
-        plot_ema1 = plot_line(df["ema1"], dataframe=df, color="green")
-        plot_crossover = plot_mark(
-            df["close"],
-            filter=df["crossover"] >= 100,
-            dataframe=df,
-            name="crossover",
-            color=PlotColor.BLUE,
-        )
-        return plot_merge(plot_ema1, plot_crossover)
 
 
 if __name__ == "__main__":

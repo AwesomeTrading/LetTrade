e43ffbff61e920b4cb8656b691f9e4c65ab5655a
Santa <namns.dev@gmail.com>
2024-10-25 16:43:25 +0700

    Backtest: add StorageBackTestCommander to store strategy result

--- lettrade/exchange/backtest/commander.py
+++ lettrade/exchange/backtest/commander.py
@@ -18,3 +18,21 @@ class BackTestCommander(Commander):
 
     def send_message(self, msg: str, **kwargs):
         logger.info("BackTestCommander send_message: %s, %s", msg, kwargs)
+
+
+class StorageBackTestCommander(Commander):
+    """
+    BackTest Commander to debug commander notify
+    """
+
+    storage = []
+
+    def start(self):
+        logger.debug("StorageBackTestCommander start")
+
+    def stop(self):
+        logger.debug("StorageBackTestCommander stop")
+
+    def send_message(self, msg: str, **kwargs):
+        logger.info("StorageBackTestCommander send_message: %s, %s", msg, kwargs)
+        self.storage.append({"message": msg, **kwargs})
